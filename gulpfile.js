d6d24b56fcb8aa2278e557e4e4cf1046102dfae7
Roni Laukkarinen <roni@laukkarinen.info>
2016-04-26 02:07:32 +0300

    More reasonable font sizes, publish rolleshark-madsonic.css

--- gulpfile.js
+++ gulpfile.js
@@ -108,7 +108,7 @@ gulp.task('styles', function() {
     .on('error', handleError('styles'))
     .pipe(prefix('last 3 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')) //adds browser prefixes (eg. -webkit, -moz, etc.)
     .pipe(minifycss({keepBreaks:false,keepSpecialComments:0,}))
-    // .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'))
     .pipe(gulp.dest('/Volumes/Root/var/madsonic/jetty/8220/webapp/style/'))
     .pipe(browserSync.stream());
     });
