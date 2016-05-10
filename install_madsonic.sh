beafb0e314904c4e97b79410bdd77034a4db5bf5
Roni Laukkarinen <roni@dude.fi>
2016-05-10 10:42:24 +0300

    Add more readable installer, update version

--- install_madsonic.sh
+++ install_madsonic.sh
@@ -1,1 +1,5 @@
-sudo cp rolleshark-madsonic.css /var/madsonic/jetty/8220/webapp/style/ && sudo cp -R icons/rolleshark /var/madsonic/jetty/8220/webapp/icons/ && sudo cp -R webfonts /var/madsonic/jetty/8220/webapp/ && sudo cp -R WEB-INF/classes/org /var/madsonic/jetty/8220/webapp/WEB-INF/classes/
+#!/bin/bash
+sudo cp rolleshark-madsonic.css /var/madsonic/jetty/8280/webapp/style/
+sudo cp -R icons/rolleshark /var/madsonic/jetty/8280/webapp/icons/
+sudo cp -R webfonts /var/madsonic/jetty/8280/webapp/
+sudo cp -R WEB-INF/classes/org /var/madsonic/jetty/8280/webapp/WEB-INF/classes/
