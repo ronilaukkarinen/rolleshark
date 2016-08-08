c5ff56eca74614d69d403e49572e46962ee9ea95
Roni Laukkarinen <roni@dude.fi>
2016-08-08 14:36:44 +0300

    Hide unnecessary album view in single album view on Madsonic

--- gulpfile.js
+++ gulpfile.js
@@ -109,7 +109,7 @@ gulp.task('styles', function() {
     .pipe(prefix('last 3 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')) //adds browser prefixes (eg. -webkit, -moz, etc.)
     .pipe(minifycss({keepBreaks:false,keepSpecialComments:0,}))
     .pipe(gulp.dest('./'))
-    .pipe(gulp.dest('/Volumes/Root/var/madsonic/jetty/8220/webapp/style/'))
+    .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
     });
 
