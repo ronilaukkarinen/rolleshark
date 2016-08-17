802a48b82172ecab1f3f5a0f7e5c8651bc230618
Roni Laukkarinen <roni@dude.fi>
2016-08-17 11:51:00 +0300

    Update installation for new version 8600

--- install_madsonic.sh
+++ install_madsonic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo cp rolleshark-madsonic.css /var/madsonic/jetty/8550/webapp/style/
-sudo cp -R icons/rolleshark /var/madsonic/jetty/8550/webapp/icons/
-sudo cp -R webfonts /var/madsonic/jetty/8550/webapp/
-sudo cp -R WEB-INF/classes/org /var/madsonic/jetty/8550/webapp/WEB-INF/classes/
+VERSION="8600"
+sudo cp rolleshark-madsonic.css /var/madsonic/jetty/${VERSION}/webapp/style/
+sudo cp -R icons/rolleshark /var/madsonic/jetty/${VERSION}/webapp/icons/
+sudo cp -R webfonts /var/madsonic/jetty/${VERSION}/webapp/
+sudo cp -R WEB-INF/classes/org /var/madsonic/jetty/${VERSION}/webapp/WEB-INF/classes/
