39f177c37ef34c02adee30d71cdf0c64a4002488
Roni Laukkarinen <roni@dude.fi>
2016-09-09 09:54:09 +0300

    Update to latest version

--- install_madsonic.sh
+++ install_madsonic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="8600"
+VERSION="8660"
 sudo cp rolleshark-madsonic.css /var/madsonic/jetty/${VERSION}/webapp/style/
 sudo cp -R icons/rolleshark /var/madsonic/jetty/${VERSION}/webapp/icons/
 sudo cp -R webfonts /var/madsonic/jetty/${VERSION}/webapp/
